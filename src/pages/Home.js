f9f7671f835506495ec287ec20c2a8b20c1d082b
Libasse Laye <78869773+Libasse-coder@users.noreply.github.com>
2023-01-17 02:47:55 +1000

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import { useMedicamentsContext } from "../hooks/useMedicamentsContext"
+import axios from 'axios';
+
+//components
+import MedicamentDetails from '../components/MedicamentDetails'
+import MedicamentForm from '../components/MedicamentForm'
+
+const Home = () => {
+	const {workouts, dispatch} = useMedicamentsContext()
+
+	useEffect(() => {
+
+		const fetchWorkouts = async () => {
+			const response = await axios.fetch('https://apinodemongo.onrender.com/api/medicaments')
+			const json = await response.json()
+
+			if (response.ok) {
+				dispatch({type: 'SET_MEDOCS', payload: json})
+			}
+		}
+
+		fetchWorkouts()
+
+	}, [])
+
+	return (
+		<div>
+
+			<MedicamentForm />
+			
+			<div className="workouts">
+
+				{workouts && workouts.map((workout) => (
+					<MedicamentDetails key={workout._id} workout={workout} />
+				))}
+
+			</div>
+			
+		</div>
+		)
+}
+
+export default Home
